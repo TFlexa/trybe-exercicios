f7afb06486f65f7349fc111e493e8091668dcbbd
TFlexa <thiago_flexa@hotmail.com>
2022-03-07 20:30:02 -0300

    Exercicios 1, 2 e 3

---
+++ Fundamentos/Bl04-Introducao-a-JavaScritp-e-Logica-de-Programacao/dia-4.1/scripts.js
@@ -0,0 +1,23 @@
+const a = 10;
+const b = 5;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+
+const num3 = 30;
+const num1 = 10;
+const num2 = 20;
+
+if (num1 > num2 && num1 > num3) {
+    console.log("num1 é o maior dos tres numeros");
+}
+else if (num2 > num3 && num2 > num1) {
+    console.log("num2 é o maior dos tres numeros");
+}
+else {
+    console.log("num3 é o maior dos tres numeros");
+};
