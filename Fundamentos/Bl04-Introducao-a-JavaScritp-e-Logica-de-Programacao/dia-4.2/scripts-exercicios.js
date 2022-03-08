2c37b15185c3ef6afc6fea47ef364ee37b76f5b6
TFlexa <thiago_flexa@hotmail.com>
2022-03-08 18:35:38 -0300

    exercicio 3

--- Fundamentos/Bl04-Introducao-a-JavaScritp-e-Logica-de-Programacao/dia-4.2/scripts-exercicios.js
+++ Fundamentos/Bl04-Introducao-a-JavaScritp-e-Logica-de-Programacao/dia-4.2/scripts-exercicios.js
@@ -1,8 +1,10 @@
 let members = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
 
-for (let i = 0; i < members.length; i += 1){
-   // console.log(members[i]);
+for (let i = 0; i < members.length; i += 1) {
+    // console.log(members[i]);
     soma += members[i];
+    media = soma/members.length;
 }
-console.log(soma);
+console.log('A soma das notas é:', soma);
+console.log('A media das notas é:', media);
