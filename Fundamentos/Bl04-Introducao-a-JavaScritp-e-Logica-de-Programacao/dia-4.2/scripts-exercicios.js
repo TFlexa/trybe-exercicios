ea346d80783342b4a507310e0ef3374975c5e38c
TFlexa <thiago_flexa@hotmail.com>
2022-03-08 18:52:07 -0300

    exercicio 5

--- Fundamentos/Bl04-Introducao-a-JavaScritp-e-Logica-de-Programacao/dia-4.2/scripts-exercicios.js
+++ Fundamentos/Bl04-Introducao-a-JavaScritp-e-Logica-de-Programacao/dia-4.2/scripts-exercicios.js
@@ -1,17 +1,22 @@
 let members = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let soma = 0;
 
-for (let i = 0; i < members.length; i += 1) {
+let maior = members[0];
+
+for (let i = 1; i < members.length; i += 1) {
+    if (maior < members[i]){
+        maior = members[i];
+    }
+}
+
+console.log('O maior array é:', maior);
     // console.log(members[i]);
-    soma += members[i];
-    media = soma/members.length;
-}
-console.log('A soma das notas é:', soma);
-console.log('A media das notas é:', media);
+    //soma += members[i];
+    //media = soma/members.length;
 
-if (media > 20){
-    console.log('valor maior que 20');
-}
-else {
-    console.log('valor menor ou igual a 20');
-}
+//console.log('A soma das notas é:', soma);
+//console.log('A media das notas é:', media);
+
+//if (media > 20){
+    //console.log('valor maior que 20');}
+//else {
+    //console.log('valor menor ou igual a 20');}
