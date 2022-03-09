708a4c1e4f1ac689b7d14863db929b418edf5821
TFlexa <thiago_flexa@hotmail.com>
2022-03-09 13:52:13 -0300

    exercicio 10

--- Fundamentos/Bl04-Introducao-a-JavaScritp-e-Logica-de-Programacao/dia-4.2/scripts-exercicios.js
+++ Fundamentos/Bl04-Introducao-a-JavaScritp-e-Logica-de-Programacao/dia-4.2/scripts-exercicios.js
@@ -1,10 +1,25 @@
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
-let divisao = 0;
 
-for (let i = 1; i < numbers.length; i += 1){
-    divisao = numbers[i] / 2;
-    console.log(divisao); 
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];{
+    numbers.sort;
+    console.log(numbers);
 }
+
+
+
+
+
+
+
+
+
+
+//let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+//let divisao = 0;
+
+//for (let i = 1; i < numbers.length; i += 1){
+  //  divisao = numbers[i] / 2;
+  //  console.log(divisao); 
+//}
 
 
 
