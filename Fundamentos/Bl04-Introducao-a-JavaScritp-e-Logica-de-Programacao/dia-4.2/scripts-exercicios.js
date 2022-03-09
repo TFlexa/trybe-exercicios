db9461c2614d157a4b3cbb88610bb1f8b63c77e1
TFlexa <thiago_flexa@hotmail.com>
2022-03-08 21:18:04 -0300

    exercicio 8

--- Fundamentos/Bl04-Introducao-a-JavaScritp-e-Logica-de-Programacao/dia-4.2/scripts-exercicios.js
+++ Fundamentos/Bl04-Introducao-a-JavaScritp-e-Logica-de-Programacao/dia-4.2/scripts-exercicios.js
@@ -1,14 +1,27 @@
-let members = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
 
-let menor = members[0];
-
-for (let i = 1; i < members.length; i+=1){
-    if (menor > members[i]){
-        menor = members[i];
-    }
+for (let i = 1; i < numbers.length; i += 1){
+    console.log(i);
 }
 
-console.log ('O menor numero do vetor é:', menor);
+
+
+
+
+
+
+
+//let members = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//let menor = members[0];
+
+//for (let i = 1; i < members.length; i+=1){
+  //  if (menor > members[i]){
+    //    menor = members[i];
+   // }
+// }
+
+//console.log ('O menor numero do vetor é:', menor);
 
 
 
