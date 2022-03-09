ad2824fed8efa699f2ddd31007ca465abf6d60ca
TFlexa <thiago_flexa@hotmail.com>
2022-03-08 21:04:33 -0300

    exercicio 6

--- Fundamentos/Bl04-Introducao-a-JavaScritp-e-Logica-de-Programacao/dia-4.2/scripts-exercicios.js
+++ Fundamentos/Bl04-Introducao-a-JavaScritp-e-Logica-de-Programacao/dia-4.2/scripts-exercicios.js
@@ -1,14 +1,30 @@
 let members = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let impar = 0
 
-let maior = members[0];
+// let maior = members[0];
 
-for (let i = 1; i < members.length; i += 1) {
-    if (maior < members[i]){
-        maior = members[i];
+for (let index = 0; index < members.length; index += 1){
+
+    if (members[index] %2 !== 0){
+    
+        impar += 1}
+    
+    if (impar === 0){
+    
+        console.log("Não foram encontrados numeros impares");
     }
-}
+        else {
+        console.log("Os seguintes numewros impares encontrados:", + impar);
+    }
+}      
+        
+    
+    
+    //if (maior < members[i]) {
+        //maior = members[i]; }
 
-console.log('O maior array é:', maior);
+
+// console.log('O maior array é:', maior);
     // console.log(members[i]);
     //soma += members[i];
     //media = soma/members.length;
